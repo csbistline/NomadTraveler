54a4dd6292eba34c0e00d2b6745939ed9193ede2
Chris Bistline <chris.bistline@gmail.com>
2019-03-11 19:23:02 -0500

    updates js

--- resources/javascript/app.js
+++ resources/javascript/app.js
@@ -5,22 +5,17 @@ $(document).ready(function () {
         // prevent submit action
         event.preventDefault();
 
-
         // grab user input
         var departCity = $("#depart-city").val().trim();
         var arriveCity = $("#arrive-city").val().trim();
         var departDate = $("#depart-date").val().trim();
-        var returnDate = $("#return-date").val().trim();
         var queryURL = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/"
             + departCity
             + "-sky/"
             + arriveCity
             + "-sky/"
             + departDate
-            + "?inboundpartialdate="
-            + returnDate;
-        console.log(queryURL);
-
+        // console.log(queryURL);
 
         var settings = {
             "async": true,
@@ -30,74 +25,29 @@ $(document).ready(function () {
             "headers": {
                 "X-RapidAPI-Key": "70b4bd87d4msh46110c5b4e371f1p11eccbjsn8595807c8dfe",
                 "Content-Type": "application/x-www-form-urlencoded",
-                "cache-control": "no-cache",
-                "Postman-Token": "60c26db9-c8ba-43ca-aaff-ddfb24e440f2"
+                // "cache-control": "no-cache",
+                // "Postman-Token": "60c26db9-c8ba-43ca-aaff-ddfb24e440f2"
             },
             "data": ""
         }
 
         $.ajax(settings).done(function (result) {
-            console.log("Result", result);
+            // console.log("Result", result);
             createItin(result);
 
         });
     });
 
-    $(document).on("click", "#search-date-btn", function () {
-
-        // prevent submit action
-        event.preventDefault();
-
-
-
-        var departCity = $("#depart-city").val().trim();
-        var arriveCity = $("#arrive-city").val().trim();
-        var departDate = $("#depart-date").val().trim();
-        var returnDate = $("#return-date").val().trim();
-        var queryURL = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/US/USD/en-US/"
-            + departCity
-            + "-sky/"
-            + arriveCity
-            + "-sky/"
-            + departDate
-            + "?inboundpartialdate="
-            + returnDate;
-        console.log(queryURL);
-
-
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": queryURL,
-            "method": "GET",
-            "headers": {
-                "X-RapidAPI-Key": "70b4bd87d4msh46110c5b4e371f1p11eccbjsn8595807c8dfe",
-                "Content-Type": "application/x-www-form-urlencoded",
-                "cache-control": "no-cache",
-                "Postman-Token": "60c26db9-c8ba-43ca-aaff-ddfb24e440f2"
-            },
-            "data": ""
-        }
-
-        $.ajax(settings).done(function (result) {
-            console.log("Result", result);
-            createItin(result);
-
-        });
-    });
-
-
+    // create flight itinerary
     function createItin(result) {
         $("#flight-sked > tbody").empty();
-
 
         var quotes = result.Quotes;
         var carriers = result.Carriers;
         var places = result.Places;
-        console.log("Quotes", quotes);
-        console.log("Carriers", carriers);
-        console.log("Places", places);
-
+        // console.log("Quotes", quotes);
+        // console.log("Carriers", carriers);
+        // console.log("Places", places);
 
         for (var i = 0; i < quotes.length; i++) {
             var currentQuote = quotes[i];
@@ -109,7 +59,7 @@ $(document).ready(function () {
             var flightPrice = Math.floor(currentQuote.MinPrice);
             var airline = lookupCarrier(currentQuote.OutboundLeg.CarrierIds[0], carriers);
 
-
+            // create row to append to table
             var flightRow = $("<tr>").append(
                 $("<td>").text(departDate), // Date
                 $("<td>").text(departTime), // Depart
@@ -122,15 +72,13 @@ $(document).ready(function () {
                 "Flights from " + departCity + " to " + arriveCity
             );
 
-            // Append the new row to the table
+            // Append the new row and label to the table
             $("#flight-cities").html(flightCities);
-
             $("#flight-sked > tbody").append(flightRow);
-
-
         };
     };
 
+    // look up places info
     function lookupCity(ID, places) {
         for (var i = 0; i < places.length; i++) {
             if (places[i].PlaceId === ID) {
@@ -139,6 +87,7 @@ $(document).ready(function () {
         };
     };
 
+    // look up carrier info
     function lookupCarrier(ID, carriers) {
         for (var i = 0; i < carriers.length; i++) {
             if (carriers[i].CarrierId === ID) {
@@ -146,5 +95,4 @@ $(document).ready(function () {
             };
         };
     };
-
 });
