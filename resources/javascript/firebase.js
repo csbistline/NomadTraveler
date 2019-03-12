daace8d687a80dfd29f3e21e7f1b275395d18de4
Ricardo Quintanar <lrq1012@gmail.com>
2019-03-12 15:55:50 -0500

    login is working redirects to landing.html

---
+++ resources/javascript/firebase.js
@@ -0,0 +1,15 @@
+var app_firbase = {};
+(function () {
+  var config = {
+    apiKey: "AIzaSyA9xnax8HPQbzeKX1sHpzmyeKa3M0djyY8",
+    authDomain: "nomad-traveler.firebaseapp.com",
+    databaseURL: "https://nomad-traveler.firebaseio.com",
+    projectId: "nomad-traveler",
+    storageBucket: "",
+    messagingSenderId: "536751174261"
+  };
+  firebase.initializeApp(config);
+
+  app_firebase = firebase;
+
+})()
